3e5e8cfd62d9ebb4375fea023cbb50bdfe65b6ec
MinSeunghyun <nadamdh1@naver.com>
2024-07-18 18:56:03 +0900

    [ADD]crud practice-read(table name:Employees_practice)

---
+++ src/main/java/com/example/sbt_final_hr/domain/model/entity/Employees_practice.java
@@ -0,0 +1,35 @@
+package com.example.sbt_final_hr.domain.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class Employees_practice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employees_practice_seq")
+    @SequenceGenerator(name = "employees_practice_seq", sequenceName = "Employees_practice_SEQ", allocationSize = 1)
+    private Long employeeID;
+    private String name;
+    private String address;
+    private Date lastProjectEndDate;
+    private Date currentProjectEndDate;
+
+    @Override
+    public String toString() {
+        return "Employees_practice [EmployeeID=" + employeeID + ", Name=" + name + ", Address=" + address + ", lastProjectEndDate=" + lastProjectEndDate + ", currentProjectEndDate=" + currentProjectEndDate + "]";
+    }
+
+
+}
---
+++ src/main/java/com/example/sbt_final_hr/domain/repository/Employees_practiceRepository.java
@@ -0,0 +1,7 @@
+package com.example.sbt_final_hr.domain.repository;
+
+import com.example.sbt_final_hr.domain.model.entity.Employees_practice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Employees_practiceRepository extends JpaRepository<Employees_practice, Long> {
+}
