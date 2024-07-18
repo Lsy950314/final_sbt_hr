2049f646f695e448da6a9f93549f169cb24dba30
lsy <curio1032@gmail.com>
2024-07-18 15:06:48 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/example/sbt_final_hr/app/SkillsController.java
    #	src/main/java/com/example/sbt_final_hr/domain/repository/SkillsRepository.java

---
+++ src/main/java/com/example/sbt_final_hr/app/MainPageController.java
@@ -0,0 +1,14 @@
+package com.example.sbt_final_hr.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/index")
+    public String getMainPage (Model model) {
+        return "mainPage/index";
+    }
+}
