9b14a7923a6cf0f7632dbbde6521f3890d1f69c0
lsy <curio1032@gmail.com>
2024-07-22 15:27:33 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/example/sbt_final_hr/app/SkillsController.java
    #	src/main/java/com/example/sbt_final_hr/domain/repository/SkillsRepository.java

--- src/main/java/com/example/sbt_final_hr/app/ProjectController.java
+++ src/main/java/com/example/sbt_final_hr/app/ProjectController.java
@@ -1,9 +1,35 @@
 package com.example.sbt_final_hr.app;
 
+import com.example.sbt_final_hr.domain.model.dto.ProjectsRequest;
+import com.example.sbt_final_hr.domain.model.entity.Projects;
+import com.example.sbt_final_hr.domain.service.ProjectsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectsService projectsService;
 
+    public ProjectController(ProjectsService projectsService) {
+        this.projectsService = projectsService;
+    }
+
+    @GetMapping("/createProject")
+    public String createProject (Model model){
+        ProjectsRequest projectsRequest = new ProjectsRequest();
+        model.addAttribute("projectsRequest", projectsRequest);
+        return "project/insertProject";
+    }
+
+    @PostMapping("/createProject")
+    public void createProject(@ModelAttribute("projectsRequest") ProjectsRequest projectsRequest, Model model) {
+        Projects project = projectsService.createProject(projectsRequest);
+        model.addAttribute("project", project);
+    }
 
 }
--- src/main/java/com/example/sbt_final_hr/domain/service/ProjectsService.java
+++ src/main/java/com/example/sbt_final_hr/domain/service/ProjectsService.java
@@ -1,5 +1,7 @@
 package com.example.sbt_final_hr.domain.service;
 
+import com.example.sbt_final_hr.domain.model.dto.ProjectsRequest;
+import com.example.sbt_final_hr.domain.model.entity.ProjectTypes;
 import com.example.sbt_final_hr.domain.model.entity.Projects;
 import com.example.sbt_final_hr.domain.repository.ProjectTypesRepository;
 import com.example.sbt_final_hr.domain.repository.ProjectsRepository;
@@ -19,6 +21,22 @@ public class ProjectsService {
 
     public List<Projects> getAllProjects() {
        return projectsRepository.findAll();
+    }
+
+    public Projects createProject(ProjectsRequest projectsRequest) {
+        ProjectTypes projectType = projectTypesRepository.findById(projectsRequest.getProjectTypeId())
+                .orElseThrow(() -> new RuntimeException("Invalid project type ID"));
+
+        // 기본 값 설정
+        if (projectsRequest.getContactPhone() == null) {
+            projectsRequest.setContactPhone("010-1234-5678");
+        }
+        if (projectsRequest.getContactName() == null) {
+            projectsRequest.setContactName("担当者");
+        }
+
+        Projects project = projectsRequest.toEntity(projectType);
+        return projectsRepository.save(project);
     }
 
 }
