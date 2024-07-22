4ddfcb60993c925b15873cb1cf158141c6ec4983
MinSeunghyun <nadamdh1@naver.com>
2024-07-22 15:28:51 +0900

    [ADD]making Employees crud-READ

--- src/main/java/com/example/sbt_final_hr/app/EmployeesController.java
+++ src/main/java/com/example/sbt_final_hr/app/EmployeesController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -49,10 +46,15 @@ public class EmployeesController {
         return "redirect:/employees";
     }
 
+
+    //READ: 직원 리스트
     @GetMapping
-    public String listEmployees(Model model) {
-        List<Employees> employeesList = employeesService.findAll();
-        model.addAttribute("employees", employeesList);
+    public String listEmployees(@RequestParam(name = "name", required = false) String name, Model model) {
+        if (name != null && !name.isEmpty()) {
+            model.addAttribute("employees", employeesService.findByName(name));
+        } else {
+            model.addAttribute("employees", employeesService.findAll());
+        }
         return "employees/employeesList";
     }
 
