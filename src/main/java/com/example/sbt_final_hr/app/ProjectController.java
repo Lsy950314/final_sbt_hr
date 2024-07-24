aa650028c948d9d97793c7dd1e796a30fadb3962
lsy <curio1032@gmail.com>
2024-07-24 11:16:30 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/example/sbt_final_hr/app/SkillsController.java
    #	src/main/java/com/example/sbt_final_hr/domain/repository/SkillsRepository.java

--- src/main/java/com/example/sbt_final_hr/app/ProjectController.java
+++ src/main/java/com/example/sbt_final_hr/app/ProjectController.java
@@ -2,13 +2,11 @@ package com.example.sbt_final_hr.app;
 
 import com.example.sbt_final_hr.domain.model.dto.ProjectRequirementsRequest;
 import com.example.sbt_final_hr.domain.model.dto.ProjectsRequest;
+import com.example.sbt_final_hr.domain.model.entity.EmployeesProjects;
 import com.example.sbt_final_hr.domain.model.entity.ProjectRequirements;
 import com.example.sbt_final_hr.domain.model.entity.Projects;
 import com.example.sbt_final_hr.domain.repository.ProjectsRepository;
-import com.example.sbt_final_hr.domain.service.ProjectRequirementsService;
-import com.example.sbt_final_hr.domain.service.ProjectTypesService;
-import com.example.sbt_final_hr.domain.service.ProjectsService;
-import com.example.sbt_final_hr.domain.service.SkillsService;
+import com.example.sbt_final_hr.domain.service.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -28,21 +27,49 @@ public class ProjectController {
     private final ProjectTypesService projectTypesService;
     private final ProjectRequirementsService projectRequirementsService;
     private final SkillsService skillsService;
+    private final EmployeesProjectsService employeesProjectsService;
 
     @Value("${google.maps.api.key}")
     private String apiKey;
 
-    public ProjectController(ProjectsService projectsService, ProjectTypesService projectTypesService, ProjectRequirementsService projectRequirementsService, SkillsService skillsService) {
+    public ProjectController(ProjectsService projectsService, ProjectTypesService projectTypesService, ProjectRequirementsService projectRequirementsService, SkillsService skillsService, EmployeesProjectsService employeesProjectsService) {
         this.projectsService = projectsService;
         this.projectTypesService = projectTypesService;
         this.projectRequirementsService = projectRequirementsService;
         this.skillsService = skillsService;
+        this.employeesProjectsService = employeesProjectsService;
     }
 
     @GetMapping("/readAllProjects")
     public String readAllProjects(Model model) {
         model.addAttribute("projects", projectsService.getAllProjects());
+
+//        model.addAttribute("employeesProjects", employeesProjectsService.getAllEmployeesProjects());
+
+        // 리스트에서 항목을 선택하면 디테일 페이지를 모달로 띄운다?
+        // 거기서 projects 가 가진 정보 외에도 필요한 것.
+        // 참여 인원, 필요 인원
+        // 현재 참여중인 인원들(진행 중인 프로젝트) or 배정 완료된 인원들(시작 전 프로젝트): 구하는 로직 동일할듯?
+        // 전체 사원-안건 테이블 전체 정보를 리스트 띄울 때 가져가지 말고,
+        // 특정 프로젝트를 선택했을 때 그에 해당하는 사원-안건 테이블을 프로젝트pk로 조회해서,
+        // 얻어낸 사원pk를 통해 사원엔티티 얻어서 모달에 보여줄 때 사용하기
+        // -> 메서드 따로 설정 ("/getEmployeeProjectsByProjectID")
+        // PROJECT_REQUIREMENTS 테이블을 프로젝트pk로 조회해서, 얻어낸 List<ProjectRequirements> 들을 표시해주기
+
         return "project/readAllProjects"; // 가상의 주소
+    }
+
+    // 모달 띄우기 전에 여기에 요청해서 어트리뷰트 가져가는 메서드
+    @GetMapping("/getEmployeeProjectsByProjectID")
+    public void getEmployeeProjectsByProjectID(@RequestParam Map<String, String> payload, Model model) {
+        Long id = Long.parseLong(payload.get("id"));
+        // 해당 프로젝트에 해당하는 사원-안건 엔티티만 얻어내고
+        List<EmployeesProjects> employeesProjects = employeesProjectsService.getEmployeesProjectByProjectId(id);
+
+
+        model.addAttribute("employeesProjects", employeesProjects);
+
+
     }
 
     @GetMapping("/createProject")
--- src/main/java/com/example/sbt_final_hr/domain/service/EmployeesProjectsService.java
+++ src/main/java/com/example/sbt_final_hr/domain/service/EmployeesProjectsService.java
@@ -1,8 +1,12 @@
 package com.example.sbt_final_hr.domain.service;
 
 import com.example.sbt_final_hr.domain.model.dto.EmployeesProjectsRequest;
+import com.example.sbt_final_hr.domain.model.entity.Employees;
+import com.example.sbt_final_hr.domain.model.entity.EmployeesProjects;
 import com.example.sbt_final_hr.domain.repository.EmployeesProjectsRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeesProjectsService {
@@ -11,8 +15,12 @@ public class EmployeesProjectsService {
         this.employeesProjectsRepository = employeesProjectsRepository;
     }
 
-    public void createEmployeesProjects(EmployeesProjectsRequest employeesProjectsRequest) {
-        employeesProjectsRepository.save(employeesProjectsRequest.toEntity());
+    public List<EmployeesProjects> getAllEmployeesProjects() {
+        return employeesProjectsRepository.findAll();
+    }
+
+    public List<EmployeesProjects> getEmployeesProjectByProjectId(Long id) {
+        return employeesProjectsRepository.findByProject_ProjectId(id);
     }
 
 }
